933d22111a76092d320bb2dbff7f3a6a58d0cbed
Omar <omar-maher@hotmail.com>
2023-05-25 13:42:51 +0300

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Omar on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
